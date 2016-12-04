5ff2000962d468f459c9740bded18c7d581f810e
deliads <delia.destefano@gmail.com>
2016-12-04 16:26:46 +0000

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,27 @@
+# Unzip the file
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+# Get the file's list
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+files<-list.files(path_rf, recursive=TRUE)
+files
+# Merges the training and the test sets to create one data set
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+# Extracts only the measurements on the mean and standard deviation for each measurement
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+# Uses descriptive activity names to name the activities in the data set
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+# Appropriately labels the data set with descriptive variable names
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+# creates a second, independent tidy data set with the average of each variable for each activity and each subject
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
